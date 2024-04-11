6633d3b4a0d50631171741b8ed7ac8208d4de6fb
Jakub Majewski <jakub.majewski@cyfroweszkoly.pl>
2024-04-11 09:31:46 +0200

    modified 'GraStanow'

--- GraStany/app/src/main/java/com/example/grastany/MainActivity.kt
+++ GraStany/app/src/main/java/com/example/grastany/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.grastany
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.grastany.ui.theme.GraStanyTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +36,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    licz()
+                    //Count()
+                    CaptainGame()
                 }
             }
         }
@@ -34,26 +45,91 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun licz(){
+fun CaptainGame(){
 
-    var liczba_klikniec = 0
+    var iloscskarbow by remember {
+        mutableIntStateOf(0)
+    }
+    var kierunek by remember {
+        mutableStateOf("Zachód")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text="Znaleziono skarbów: $iloscskarbow")
+        
+        Spacer(modifier = Modifier.height(10.dp))
+        
+        Text("Kierunek: $kierunek")
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = {
+            kierunek = "Zachód"
+            if (Random.nextBoolean()){
+                iloscskarbow += 1
+            }
+        }) {
+            Text(text = "Płyń za Zachód")
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+            kierunek = "Wschód"
+            if (Random.nextBoolean()){
+                iloscskarbow += 1
+            }
+        }) {
+            Text(text = "Płyń za Wschód")
+        }
+        Button(onClick = {
+            kierunek = "Północ"
+            if (Random.nextBoolean()){
+                iloscskarbow += 1
+            }
+        }) {
+            Text(text = "Płyń za Północ")
+        }
+        Button(onClick = {
+            kierunek = "Południe"
+            if (Random.nextBoolean()){
+                iloscskarbow += 1
+            }
+        }) {
+            Text(text = "Płyń za Południe")
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Aktualnie płyniesz w kierunku: $kierunek")
+    }
+}
+
+@Composable
+fun Count(){
+
+    var clickCounter by remember {
+        mutableIntStateOf(0)
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "liczba kliknieć $liczba_klikniec")
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "kliknij mnie")
+        Text(text = "Amount of clicks $clickCounter")
+        Button(onClick = {
+            clickCounter += 1
+            Log.i("click", "$clickCounter")
+        }) {
+            Text(text = "Click me")
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CaptainGamePreview() {
     GraStanyTheme {
-        licz()
+        CaptainGame()
     }
 }
