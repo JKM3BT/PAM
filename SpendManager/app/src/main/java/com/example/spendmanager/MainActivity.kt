97c2c3c30e79485415db3b1fc1ad131196280af7
Jakub Majewski <jakub.majewski@cyfroweszkoly.pl>
2024-06-12 19:59:09 +0200

    'SpendManager' is done

--- SpendManager/app/src/main/java/com/example/spendmanager/MainActivity.kt
+++ SpendManager/app/src/main/java/com/example/spendmanager/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -18,7 +20,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -48,14 +50,26 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SpendManager() {
     var sum by remember {
-        mutableFloatStateOf(0F)
+        mutableDoubleStateOf(0.00)
     }
     var item by remember {
         mutableStateOf("")
     }
 
-    fun sumUp() : Float{
-        sum += item.toFloat()
+    fun sumUpPlus() : Double{
+        sum += item.toDouble()
+        item = ""
+        return sum
+    }
+
+    fun sumUpMinus() : Double {
+        if (sum > 0.00) {
+            sum -= item.toDouble()
+        }
+        if (sum < 0.00){
+            sum = 0.00
+        }
+        item = ""
         return sum
     }
     Column(
@@ -67,17 +81,28 @@ fun SpendManager() {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(value = item, onValueChange = {item})
+        OutlinedTextField(value = item, onValueChange = { item = it})
 
         Spacer(modifier = Modifier.height(20.dp))
-        
-        Button(onClick = {sumUp()}) {
-            Text(text = "Dodaj do listy")
-            Icon(imageVector = Icons.Default.Add, contentDescription = "plus")
+
+        Row {
+            Button(onClick = {sumUpPlus()}) {
+                Text(text = "Dodaj do sumy")
+                Icon(imageVector = Icons.Default.Done, contentDescription = "plus")
+            }
+
+            Button(onClick = {sumUpMinus()}) {
+                Text(text = "Odejmij od sumy")
+                Icon(imageVector = Icons.Default.Close, contentDescription = "Minus")
+            }
         }
         
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(text = "Łączna suma: ")
+
         Spacer(modifier = Modifier.height(20.dp))
-        
+
         Text(text = "$sum zł")
     }
 }
