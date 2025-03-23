e4574820560809d44bf9d0b6e3343a8258364f95
Jakub Majewski <jakubmajewski@smartoakprojects.com>
2025-03-23 22:49:14 +0100

    CookBook is mostly finished. Only problem is implementing items from shared preferences into an array

---
+++ CookBook/app/src/main/java/com/example/cookbook/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.cookbook
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (savedInstanceState == null) {
+            replaceFragment(MenuFragment())
+
+        }
+    }
+}
